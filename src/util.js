aa8d053bcd2a9000bbed7763fc11930ed0ab1dcd
Marvel Mathew <marvel@ampme.com>
2017-09-21 14:54:54 -0400

    Log to single line

--- src/util.js
+++ src/util.js
@@ -9,7 +9,7 @@ const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
       handleExceptions: true,
-      json: true,
+      stringify: true,
     }),
   ],
 });
